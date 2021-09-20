9ec5d2cd63e5c9ee6196dff0761e7ed0f08d1e91
gcsxdu <gcsxdu@gmail.com>
2021-09-20 23:20:31 +0800

    Add files via upload
    transfer a code snippet to the format of VScode requires

---
+++ vscode_snippets_gen.py
@@ -0,0 +1,25 @@
+blanks_per_tab = 6
+with open('des.txt', 'w+') as f_des:
+    f_des.write('"name": {\n\t"prefix": "prefix",\n\t"body": [\n')
+    cnt = 1
+    with open('src.txt') as f_src:
+        lines = f_src.readlines()
+        for line in lines:
+            s = '"'
+            for i in range(len(line)):
+                if line[i] == "\\": #遇到反斜杠，得转义一下
+                    s += '\\\\'
+                elif line[i] == "\n": #末尾的回车键不管
+                    continue
+                elif line[i] == "}":
+                    if i > 0 and line[i-1] == '{':
+                        s += "$"+str(cnt)
+                        cnt += 1;
+                    s += "}"
+                else:
+                    s += line[i]
+            s += '", \n'
+            s = "\t\t"+s.replace(" "*blanks_per_tab, "\\t") #统一处理tab键，空格数视情况而定
+            f_des.write(s)
+
+    f_des.write('\t],\n\t"description": "discription"\n}')
